47ea8652b52a6d143a774561a7faec2e5976900d
shad6i <alex.trofimuk1998@gmail.com>
2024-09-30 19:15:39 +0300

    added API method to update currency

---
+++ app/Http/Controllers/Api/CurrencyRateController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\CurrencyService;
+use Illuminate\Http\JsonResponse;
+
+class CurrencyRateController extends Controller
+{
+    protected $currencyRateService;
+
+    public function __construct(CurrencyService $currencyRateService)
+    {
+        $this->currencyRateService = $currencyRateService;
+    }
+
+    // Метод для обновления курсов через API
+    public function updateRates(): JsonResponse
+    {
+        try {
+            $this->currencyRateService->fetchRates();
+            return response()->json(['status' => 'success', 'message' => 'Currency rates updated successfully'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()], 500);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\CurrencyRateController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('admin')->as('integrations.')->group(function () {
+Route::prefix('admin')->as('admin.')->group(function () {
     Route::get('currencies', [App\Http\Controllers\Admin\CurrencyController::class, 'index'])->name('admin.currencies.index');
 });
 
+Route::prefix('api')->as('api.')->group(function () {
+    Route::get('/currency/update', [CurrencyRateController::class, 'updateRates']);
+});
